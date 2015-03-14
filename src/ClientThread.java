879187b66c790e389380dd262ee6bf2d4bc4c239
Thierry van Ekeren <t.van.ekeren@anva.nl>
2015-03-14 11:05:39 +0100

    Working on handling settings post, it works but not secure atm

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -1,3 +1,5 @@
+import sun.security.krb5.Config;
+
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -14,16 +16,19 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ClientThread implements Runnable {
 	
 	public static Integer bufferSize = 128;
 	public static String SERVER_OPENEDSOCKET = "Socket opened: ";
 	public static String SERVER_CLOSINGSOCKET = "Closing socket: ";
-	public static String SERVER_DOCUMENTROOT = "/home/johan/Desktop/dh14";
 
 	public static String ERROR_CLIENT_CLOSED_CONNECTION = "Error : Client closed connection.";
 	public static String ERROR_CLIENT_FILENOTEXISTS = "File does not exist.";
+
+    public static String ADMIN_URI = "/adminerino";
 
 	/** The socket on which the client is connected */
 	private Socket socket;
@@ -57,8 +62,6 @@ public class ClientThread implements Runnable {
 	 */
 	@Override
 	public void run() {
-
-        /*
 		try {
 			printLine(SERVER_OPENEDSOCKET + getSocketInfo(), 0);
 
@@ -68,16 +71,18 @@ public class ClientThread implements Runnable {
 			bufferedReader = new BufferedReader(inputStreamReader);
 			printWriter = new PrintWriter(socket.getOutputStream(), true);
 
-			String line;
 
-			// process client input
-			while ((line = bufferedReader.readLine()) != null) {
-				processInput(line);
-			}
+            ArrayList<String> lines = readTillEmptyLine();
+            // only process when there are lines
+            if(lines.size() > 0) {
 
-			printLine(SERVER_CLOSINGSOCKET + getSocketInfo(), 0);
+                //read first line
+                readRequestMethod(lines);
+            }
 
-			// close when there is no more input
+			//printLine(SERVER_CLOSINGSOCKET + getSocketInfo(), 0);
+
+            // close when there is no more input
 			inputStreamReader.close();
 			bufferedReader.close();
 			socket.close();
@@ -88,34 +93,183 @@ public class ClientThread implements Runnable {
 			// TODO
 			e.printStackTrace();
 		}
-		*/
 	}
+
+    /**
+     * Handles user input
+     *
+     * @param lines
+     * @throws IOException
+     */
+    protected void readRequestMethod(ArrayList<String> lines) throws IOException, Exception {
+        String line = lines.get(0);
+        printLine(line, 2);
+
+        // split the input string by whitespace
+        String[] lineSplit = line.split(" ");
+
+        // should have 3 indexes (method uri protocol)
+        if (lineSplit.length < 2) {
+            return;
+        }
+
+        String method = lineSplit[0];
+        String uri = lineSplit[1];
+        String protocol = lineSplit[2];
+        if(method.equals("GET")) {
+            handleGet(uri, protocol);
+        } else if (method.equals("POST")) {
+            Integer contentLength = 0;
+            final String contentHeader = "Content-Length: ";
+            String contentLengthString = lines.get(lines.size()-1);
+            if (contentLengthString.startsWith(contentHeader)) {
+                contentLength = Integer.parseInt(contentLengthString.substring(contentHeader.length()));
+            }
+            handlePost(uri, protocol, contentLength);
+        }
+    }
 
 	/**
 	 * Temporary GET handler
 	 * 
-	 * @param filePath
+	 * @param uri
+	 * @param protocol
 	 * @throws IOException
 	 */
-	private void handleGet(String filePath) throws IOException {
-		String fullPath = SERVER_DOCUMENTROOT + filePath;
+	private void handleGet(String uri, String protocol) throws IOException {
+        // if ask for root get default page (TODO accept multiple defaults)
+        if(uri == "/") {
+            uri = ConfigPropertyValues.get("defaultpage");
+        }
+
+        String fullPath = ConfigPropertyValues.get("docroot") + uri;
+
+        //TODO if fullPath is not permitted return 403
+        //TODO implement content-length
+        //TODO implement content-type
+        //TODO implement http status code
+
+        printLine(fullPath, 0);
 		File file = new File(fullPath);
 
 		if (fileExists(file)) {
-			sendLine("OK");
+            // Print Headers
+            //TODO find better solution
+            FileResource fr = new FileResource(fullPath);
+            sendResponseHeader(200, fr.getByteSize());
+
+            //Print double linebreak to tell browser content is starting
+            sendLine("");
+            sendLine("");
 			sendFile(fullPath);
-		} else {
-			printLine(ERROR_CLIENT_FILENOTEXISTS, 3);
+            return;
 		}
+
+        printLine(uri, 1);
+
+        if(uri.equals(ADMIN_URI)) {
+            String form = getManageForm();
+            sendResponseHeader(200, form.length());
+
+            //Print double linebreak to tell browser content is starting
+            sendLine("");
+            sendLine("");
+            sendLine(form);
+            return;
+        }
+
+        // No action to do with this request
+        sendResponseHeader(404, 0);
+        printLine(ERROR_CLIENT_FILENOTEXISTS, 3);
 	}
 	
-	protected void handlePost(String fileName) throws IOException, Exception {
-		sendLine("OK");
-		int totalSize = waitForFileSize();
-		//sendLine("Yes");
-		receiveFile(totalSize, fileName);
-		sendLine("Thx");
+	protected void handlePost(String uri, String protocol, int contentLength) throws IOException, Exception {
+        printLine(uri,2);
+
+        HashMap<String, String> post = new HashMap<String, String>();
+        if(contentLength > 0) {
+            char[] data = new char[contentLength];
+            bufferedReader.read(data);
+            String[] rawPost = (new String(data)).split("&");
+            for(int i = 0; i < rawPost.length; i++) {
+                String[] split = rawPost[i].split("=");
+                String value = null;
+                // no value given
+                if(split.length == 2) {
+                    value = split[1];
+                }
+                post.put(split[0], value);
+            }
+        }
+
+        if(uri.equals(ADMIN_URI)) {
+            printLine(post.toString(),2);
+            // UGLY :D
+            if(post.containsKey("port")) {
+                ConfigPropertyValues.set("port", new Integer(post.get("port")).toString());
+            }
+            if(post.containsKey("docroot")) {
+                ConfigPropertyValues.set("docroot", post.get("docroot"));
+            }
+            if(post.containsKey("defaultpage")) {
+                ConfigPropertyValues.set("defaultpage", post.get("defaultpage"));
+            }
+            if(post.containsKey("directorybrowsing")) {
+                if(post.get("directorybrowsing").equals("on")) {
+                    ConfigPropertyValues.set("directorybrowsing", "1");
+                } else {
+                    ConfigPropertyValues.set("directorybrowsing", "0");
+                }
+            }
+            ConfigPropertyValues.write();
+            sendResponseHeader(203, 0);
+
+            //Close socket so a new one will start on another port
+            try {
+                socket.close();
+                fileServer.closeServer();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+
 	}
+
+    private ArrayList<String> readTillEmptyLine() throws IOException {
+        ArrayList<String> lines = new ArrayList<String>();
+        String line = bufferedReader.readLine();
+
+        // first read all header lines
+        while (line != null && line.length() > 0) {
+            lines.add(line);
+            line = bufferedReader.readLine();
+        }
+        return lines;
+    }
+
+
+    private void sendResponseHeader(int statusCode, int contentLength) {
+        String status = null;
+        switch(statusCode) {
+            case 200:
+                status = "200 OK";
+                break;
+            case 203:
+                status = "203 NO CONTENT";
+                break;
+            case 404:
+                status = "404 NO FOUND";
+                break;
+        }
+        try {
+            sendLine("HTTP/1.1 "+status);
+            sendLine("Content-Type: text/html");
+            sendLine("Content-Length: "+contentLength);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 	private Boolean fileExists(File file) {
 		if (file.exists() && !file.isDirectory()) {
@@ -129,46 +283,6 @@ public class ClientThread implements Runnable {
 		return retVal.replace("/", "");
 	}
 
-	/**
-	 * Handles user input
-	 * 
-	 * @param line
-	 * @throws IOException
-	 */
-	protected void processInput(String line) throws IOException, Exception {
-
-		String command, parameter;
-
-		// split the input string by whitespace
-		String[] lineSplit = line.split(" ");
-
-		// should have 3 indexes
-		if (lineSplit.length < 2) {
-			return;
-		}
-
-		command = lineSplit[0];
-		parameter = lineSplit[1];
-
-		printLine(line, 2);
-
-        /**
-		switch (command) {
-		case "GET":
-			handleGet(parameter);
-			break;
-
-		case "POST":
-			handlePost(parameter);
-			break;
-
-		case "Thx":
-			// nutteloos
-			break;
-		}
-         */
-	}
-
 	protected void sendLine(String message) throws IOException {
 		sendLine(message, false);
 	}
@@ -176,34 +290,10 @@ public class ClientThread implements Runnable {
 	protected void sendLine(String message, boolean toOut) throws IOException {
 		printWriter.println(message);
 
-		if (!toOut) {
+        // BUG in old codebase, toOut suggests that it will be outputted but if toOut is true it doesn't output anything
+		if (toOut) {
 			printLine(message, 1);
 		}
-	}
-
-	protected void printLine(String message, Integer sendType) {
-		String fm = "";
-
-		switch (sendType) {
-		default:
-		case 0:
-			fm += "[SERVER] ";
-			break;
-		case 1:
-			fm += "[SEND]   ";
-			break;
-		case 2:
-			fm += "[RECV]   ";
-			break;
-		case 3:
-			fm += "[ERROR]  ";
-			break;
-		}
-
-		fm += "[" + getSocketInfo() + "] ";
-		fm += message;
-
-		System.out.println(fm);
 	}
 
 	/**
@@ -226,9 +316,6 @@ public class ClientThread implements Runnable {
 		DataOutputStream dos = new DataOutputStream(os);
 
 		// send filesize to client
-		String byteSize = new Integer(fr.getByteSize()).toString();
-		sendLine(byteSize, false);
-
 		printLine(filePath, 1);
 
 		// tries to read the complete file into the inputstream
@@ -244,54 +331,96 @@ public class ClientThread implements Runnable {
 		dis.close();
 	}
 	
-	protected void receiveFile(int totalSize, String fileName) throws IOException {
-	    int currentTot = 0;
-	    int bytesRead;
-	    byte [] bytearray  = new byte [bufferSize];
-	    
-	    InputStream is = socket.getInputStream();
-	    BufferedOutputStream bos;
-		bos = getFileOutputStream(SERVER_DOCUMENTROOT+fileName);
-    
-	    int readLength = bufferSize;
-	    while(currentTot < totalSize) {
-	       bytesRead = is.read(bytearray, 0, readLength);
-	       
-	       bos.write(bytearray,  0, bytesRead);
-	       bos.flush();
-	       
-	       bytearray = new byte [bufferSize];
-	       currentTot = currentTot + bytesRead;
-
-	       if((currentTot+bufferSize) > totalSize) {
-	    	   readLength = totalSize - currentTot;
-	       }
-	    } 
-	}
+//	protected void receiveFile(int totalSize, String fileName) throws IOException {
+//	    int currentTot = 0;
+//	    int bytesRead;
+//	    byte [] bytearray  = new byte [bufferSize];
+//
+//	    InputStream is = socket.getInputStream();
+//	    BufferedOutputStream bos;
+//		bos = getFileOutputStream(SERVER_DOCUMENTROOT+fileName);
+//
+//	    int readLength = bufferSize;
+//	    while(currentTot < totalSize) {
+//	       bytesRead = is.read(bytearray, 0, readLength);
+//
+//	       bos.write(bytearray,  0, bytesRead);
+//	       bos.flush();
+//
+//	       bytearray = new byte [bufferSize];
+//	       currentTot = currentTot + bytesRead;
+//
+//	       if((currentTot+bufferSize) > totalSize) {
+//	    	   readLength = totalSize - currentTot;
+//	       }
+//	    }
+//	}
 	
 	private BufferedOutputStream getFileOutputStream(String fileName) throws FileNotFoundException {
 		FileOutputStream fos = new FileOutputStream(fileName);
 	    BufferedOutputStream bos = new BufferedOutputStream(fos);
 	    return bos;
 	}
-	
-	private int waitForFileSize() throws Exception {
-		String line = bufferedReader.readLine();
-		return Integer.parseInt(line);
-	}
 
 	protected void listFolderContent() {
 		// TODO
 	}
 
-    protected void resetPort() {
-        ConfigPropertyValues.set("port", "8012");
-        System.out.println("CONNNECT");
-        try {
-            socket.close();
-            fileServer.closeServer();
-        } catch (IOException e) {
-            e.printStackTrace();
+    protected void printLine(String message, Integer sendType) {
+        String fm = "";
+
+        switch (sendType) {
+            default:
+            case 0:
+                fm += "[SERVER] ";
+                break;
+            case 1:
+                fm += "[SEND]   ";
+                break;
+            case 2:
+                fm += "[RECV]   ";
+                break;
+            case 3:
+                fm += "[ERROR]  ";
+                break;
         }
+
+        fm += "[" + getSocketInfo() + "] ";
+        fm += message;
+
+        System.out.println(fm);
+    }
+
+    protected String getManageForm(){
+        String admin =  "<form method=\"post\" action=\"" +
+                ADMIN_URI +
+                "\">\n" +
+                "<table>\n" +
+                "<thead>\n" +
+                "    <tr><th>SuperServer</th><th class=\"right\">Control Panel</th></tr>\n" +
+                "</thead>\n" +
+                "<tbody>\n" +
+                "    <tr><td>Web port:</td><td><input name=\"port\" value=\"" +
+                ""+ ConfigPropertyValues.get("port")+"" +
+                "\" type=\"text\"></td></tr>\n" +
+                "    <tr><td>Webroot:</td><td><input name=\"docroot\" value=\"" +
+                ""+ ConfigPropertyValues.get("docroot")+"" +
+                "\" type=\"text\"></td></tr>\n" +
+                "    <tr><td>Default page:</td><td><input name=\"defaultpage\" value=\"" +
+                ""+ ConfigPropertyValues.get("defaultpage")+"" +
+                "\" type=\"text\"></td></tr>\n" +
+                "    <tr><td>Directory browsing</td><td><input name=\"directorybrowsing\" type=\"checkbox\"";
+                if(ConfigPropertyValues.get("directorybrowsing").equals("1")) {
+                    admin += " checked=\"checked\"";
+                }
+
+                admin += "></td></tr>\n" +
+                "    <tr><td><input value=\"Show Log\" type=\"button\"></td>\n" +
+                "        <td class=\"right\"><input value=\"OK\" type=\"submit\"></td>\n" +
+                "    </tr>\n" +
+                "</tbody>\n" +
+                "</table>\n" +
+                "</form>";
+        return admin;
     }
 }
