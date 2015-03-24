5dc1b0cd189dd37bc3ea6879e49ee6109e286c74
Thierry van Ekeren <t.van.ekeren@anva.nl>
2015-03-24 13:08:10 +0100

    SSL fix

--- src/nl/tbearfrobsie/dh15/webserver/FileServer.java
+++ src/nl/tbearfrobsie/dh15/webserver/FileServer.java
@@ -13,23 +13,23 @@ import java.io.IOException;
 import java.security.KeyStore;
 
 public class FileServer {
-	
+
 	public static String SERVER_STARTED = "Server started on port: ";
 	public static String SERVER_CLIENT_ACCEPTED = "Accepted connection : ";
 	public static String SERVER_CLIENT_DISCONNECTED = "Client disconnected : ";
 	public static String SERVER_ERROR_COULDNOTSTART = "Could not start server.";
 	public static String SERVER_ERROR_COULDNOTREADCONFIG = "Could not read config.";
-    public static String SERVER_ERROR_EXCEPTION = "EXCEPTION THROWN IN THREAD";
-    
-    public static String CERTIFICATE_TYPE = "SunX509";
-    public static String CERTIFICATE_CONTEXT = "TLS";
-	
+	public static String SERVER_ERROR_EXCEPTION = "EXCEPTION THROWN IN THREAD";
+
+	public static String CERTIFICATE_TYPE = "SunX509";
+	public static String CERTIFICATE_CONTEXT = "TLS";
+
 	/** ServerSocket */
 	private SSLServerSocket serverSocket;
-	
+
 	/** Is the server running? */
 	public boolean isRunning = true;
-	
+
 	/**
 	 * Starts the fileserver
 	 * 
@@ -37,73 +37,71 @@ public class FileServer {
 	 */
 	public void startServer() throws IOException
 	{
-        // Loop to restart server after exception
+		// Loop to restart server after exception
 		while(true) {
-            // setup serversocket with port
-            try {
-                int port = new Integer(ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_PORT));
-                System.out.println(SERVER_STARTED + port);
-                
-                // SSL even uitgezet @ testen
-                createSSLServerSocket();
-                //serverSocket = new ServerSocket(port);
-                
-                while (isRunning) {
-                    // accept connections
-                	SSLSocket clientSocket = (SSLSocket) serverSocket.accept();
-                    //Socket clientSocket = serverSocket.accept();
+			// setup serversocket with port
+			try {
+				int port = new Integer(ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_PORT));
+				System.out.println(SERVER_STARTED + port);
 
-                    // start new thread for incoming connection
-                    Thread t = new Thread(new ClientThread(clientSocket, this));
-                    t.start();
-                }
-                this.closeServer();
-            } catch (Exception e) {
-                // restart server
-            }
-        }
+				// SSL even uitgezet @ testen
+				createSSLServerSocket();
+				//serverSocket = new ServerSocket(port);
+
+				while (isRunning) {
+					// accept connections
+					SSLSocket clientSocket = (SSLSocket) serverSocket.accept();
+					//Socket clientSocket = serverSocket.accept();
+
+					// start new thread for incoming connection
+					Thread t = new Thread(new ClientThread(clientSocket, this));
+					t.start();
+				}
+				this.closeServer();
+			} catch (Exception e) {
+				// restart server
+			}
+		}
 	}
 
-    private void createSSLServerSocket() {
-        try{
-            int port = new Integer(ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_PORT));
-            String STORETYPE 		= (String) ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_CERT_STORETYPE);
-            String KEYSTORE 		= (String) ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_CERT_KEYSTORE);
-            String STOREPASSWORD 	= (String) ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_CERT_STOREPASSWORD);
-            String KEYPASSWORD 		= (String) ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_CERT_KEYPASSWORD);
+	private void createSSLServerSocket() {
+		try{
+			int port = new Integer(ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_PORT));
+			String KEYSTORE 		= (String) ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_CERT_KEYSTORE);
+			String STOREPASSWORD 	= (String) ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_CERT_STOREPASSWORD);
+			String KEYPASSWORD 		= (String) ConfigPropertyValues.get(ConfigPropertyValues.CONFIG_KEY_CERT_KEYPASSWORD);
 
-            KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-            ks.load(new FileInputStream(KEYSTORE), "123456".toCharArray());
-            
-            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            kmf.init(ks, "123456".toCharArray());
-            
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(kmf.getKeyManagers(), null, null);
-            
-            SSLServerSocketFactory ssf = sc.getServerSocketFactory();
-            
-            //Start de socket
-	           try {
-	            this.serverSocket = (SSLServerSocket) ssf.createServerSocket(port);
-	           }	
-	           catch (Exception e) {
-	               throw new Exception("Kan de webserver niet starten op poort " + port + ".");
-	           }
+			KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+			ks.load(new FileInputStream(KEYSTORE), KEYPASSWORD.toCharArray());
 
-        } catch( Exception e ) {
-            e.printStackTrace();
-        }
-    }
+			KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+			kmf.init(ks, STOREPASSWORD.toCharArray());
+
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(kmf.getKeyManagers(), null, null);
+
+			SSLServerSocketFactory ssf = sc.getServerSocketFactory();
+
+			//Start de socket
+			try {
+				this.serverSocket = (SSLServerSocket) ssf.createServerSocket(port);
+			} catch (Exception e) {
+				throw new Exception("Kan de webserver niet starten op poort " + port + ".");
+			}
+
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
+	}
 
 
-    public void closeServer() {
-        try {
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public void closeServer() {
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	/**
 	 * Main
 	 * 
@@ -111,21 +109,21 @@ public class FileServer {
 	 */
 	public static void main(String[] args)
 	{
-        // init config
-        try {
-            ConfigPropertyValues.load();
-        } catch (IOException e) {
-            System.out.println(SERVER_ERROR_COULDNOTREADCONFIG);
-            System.exit(0);
-        }
+		// init config
+		try {
+			ConfigPropertyValues.load();
+		} catch (IOException e) {
+			System.out.println(SERVER_ERROR_COULDNOTREADCONFIG);
+			System.exit(0);
+		}
 		// init file server
 		FileServer fileServer = new FileServer();
-		
+
 		try {
 			// start file server
 			fileServer.startServer();
 		} catch (Exception e) {
-            e.printStackTrace();
+			e.printStackTrace();
 			System.out.println(SERVER_ERROR_COULDNOTSTART);
 			System.exit(0);
 		}
